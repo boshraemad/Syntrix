f48b9414376c3be10c61eaf9f4d13c4d3bd6bccd
boshraemad <boshraemad2004@icloud.com>
2026-01-28 14:59:34 +0200

    add logout button to navbar

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Logo from './Logo'
 import User from './User'
 import SearchBar from './SearchBar'
+import { Link } from 'react-router-dom'
 export default function NavBar() {
   return (
     <div className="bg-second h-11.25 flex justify-between items-center p-2">
         <Logo/>
         <SearchBar/>
+        <div className='flex items-center gap-2'>
+        <Link to="/login"><button className='px-6 py-0.5 bg-[#DCE2EA] cursor-pointer rounded-xl text-sm'>Logout</button></Link>
         <User/>
+        </div>
     </div>
   )
 }
