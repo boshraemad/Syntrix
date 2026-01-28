e091b27d6220b0c57f21d338bbb3f3bc9dea6d40
boshraemad <boshraemad2004@icloud.com>
2026-01-28 15:44:27 +0200

    fix discover page bar-1

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 
 export default function Discover() {
   return (
-    <div className='flex items-center justify-between'>
-      <span className='bg-[#1F2029] text-font p-2.5'>Discover</span>
-      <div className='flex items-center gap-2.5'>
-      <ul className='flex items-center gap-3.5'>
+<>
+<div className='flex items-center justify-between'>
+      <span className='bg-[#1F2029] text-font px-2.5 py-1'>Discover</span>
+      <div className='flex items-center gap-3'>
+      <ul className='flex items-center gap-5'>
         <li>New</li>
         <li>Open</li>
         <li>Share</li>
@@ -15,5 +16,6 @@ export default function Discover() {
       <button className='flex items-center justify-center gap-2 bg-font text-third w-24.5 px-2.5 py-1 rounded-sm font-semibold cursor-pointer'><Save className='w-4 h-4'/><span className='text-xs'>Save</span></button>
       </div>
     </div>
+</>
   )
 }
